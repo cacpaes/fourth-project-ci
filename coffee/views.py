48a4db87697757615cbfaaef08d6b33157397359
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-03 15:53:02 +0000

    Add CoffeeIndex class to index html

--- coffee/views.py
+++ coffee/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import CoffeePost
 
-# Create your views here.
+
+class CoffeeIndex(generic.ListView):
+    model = CoffeePost
+    queryset = CoffeePost.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
