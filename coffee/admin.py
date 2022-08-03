e53a36c82c6183a991b77d19ddccb32865ccf7be
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-03 13:47:22 +0000

    Update admin model

--- coffee/admin.py
+++ coffee/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Coffee
+from .models import CoffeePost
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Coffee)
-class CoffeeAdmin(SummernoteModelAdmin):
 
+@admin.register(CoffeePost)
+class CoffeePostAdmin(SummernoteModelAdmin):
+
+    list_display = ('coffee_name', 'coffee_origin', 'coffee_brand', 'created_on')
+    search_fields = ['coffee_name', 'coffee_origin', 'coffee_brand']
+    prepopulated_fields = {'slug': ('coffee_name', 'coffee_brand')}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('coffee_content')
