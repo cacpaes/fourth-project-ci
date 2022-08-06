961fb27e8faa3f41f3c367974bc8f61562e1be41
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-06 19:29:38 +0000

    Add new class CreateCoffee

--- coffee/urls.py
+++ coffee/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.CoffeeIndex.as_view(), name='home'),
+    path('add-coffee', views.CreateCoffee.as_view(), name='create_coffee'),
 ]
