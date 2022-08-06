0ceeb2a4fdc0c13a53341cd5b377cefbf24282db
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-06 19:26:53 +0000

    Add path to add-coffee for create_coffee

--- coffee/views.py
+++ coffee/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import CoffeePost
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, get_object_or_404, reverse
+from django.contrib import messages
+from django.urls import reverse_lazy
+
 
 
 class CoffeeIndex(generic.ListView):
@@ -8,3 +13,18 @@ class CoffeeIndex(generic.ListView):
     queryset = CoffeePost.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class CreateCoffee(LoginRequiredMixin, generic.CreateView):
+    model = CoffeePost
+    fields = ['coffee_name', 'coffee_origin', 'coffee_brand', 'coffee_content', 'coffee_image']
+    template_name ='add_coffee.html'
+    success_url =reverse_lazy('home')
+
+    def form_valid(self,form):
+        form.instance.username = self.request.user
+        form.instance.status = 1
+        return super(CreateCoffee, self).form_valid(form)
+
+
+
