9878b2cb8ce4321cae50f9da4d6a065357fa04ab
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-09 11:02:36 +0000

    Add urls edit-coffe and slug in path

--- coffee/urls.py
+++ coffee/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.CoffeeIndex.as_view(), name='home'),
     path('add-coffee', views.CreateCoffee.as_view(), name='create_coffee'),
+    path('edit-coffee/<slug:slug>/', views.EditCoffee.as_view(), name='edit_coffee'),
 ]
