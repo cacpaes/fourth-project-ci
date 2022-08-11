5ee3fd419e1d345d765537a30a7d41528dd2ced2
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-11 12:15:28 +0000

    Add urls delete-coffee and slug in path

--- coffee/urls.py
+++ coffee/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.CoffeeIndex.as_view(), name='home'),
     path('add-coffee', views.CreateCoffee.as_view(), name='create_coffee'),
     path('edit-coffee/<slug:slug>/', views.EditCoffee.as_view(), name='edit_coffee'),
+    path('delete-coffee/<slug:slug>/', views.DeleteCoffee.as_view(), name='delete_coffee'),
 ]
