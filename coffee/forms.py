c91c35008d5314d9436cc9e9862b8d991d642361
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-20 22:11:11 +0000

    Setup coffee forms

--- coffee/forms.py
+++ coffee/forms.py
@@ -6,14 +6,20 @@ class CoffeePostForm(forms.ModelForm):
     """
     Form class to create coffee post.
     """
-    brand = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Nespresso, ...'}))
-    origin = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Minas Gerais, ...'}))
-    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Colombiano, ...'}))
+    coffee_brand = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Nespresso, ...', 'class': 'form-control'}), label="Brand")
+    coffee_origin = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Minas Gerais, ...', 'class': 'form-control'}), label="Origin")
+    coffee_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Colombiano, ...', 'class': 'form-control'}), label="Name")
+    coffee_content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Ex: Content Coffee, ...', 'class': 'form-control'}), label="Content")
+    coffee_image = forms.FileField(widget=forms.FileInput(attrs={'accept':'image/*'}))
+
+
 
     class Meta:
         model = CoffeePost
         fields = [
             'coffee_brand',
-            'origin',
-            'name',
+            'coffee_origin',
+            'coffee_name',
+            'coffee_content',
+            'coffee_image'
         ]
