a3d850e4c120eece6426d751ce68a1f5757a7240
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-23 13:02:45 +0000

    modified some styles

--- coffee/views.py
+++ coffee/views.py
@@ -13,12 +13,12 @@ class CoffeeIndex(generic.ListView):
     model = CoffeePost
     queryset = CoffeePost.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 6
+    paginate_by = 4
 
 class CoffeeMyarea(generic.ListView):
     model = CoffeePost
     template_name = 'my-area.html'
-    paginate_by = 6
+    paginate_by = 4
     def get_queryset(self):
         return CoffeePost.objects.filter(username=self.request.user).order_by('-created_on')
 
