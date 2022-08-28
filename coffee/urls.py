f17d6112271253e241660450e09961ced264d615
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-08-28 00:28:36 +0000

    adjust image page index.html

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,6 +1,7 @@
 from . import views 
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.CoffeeIndex.as_view(), name='home'),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('add-coffee', views.CreateCoffee.as_view(), name='create_coffee'),
     path('edit-coffee/<slug:slug>/', views.EditCoffee.as_view(), name='edit_coffee'),
     path('delete-coffee/<slug:slug>/', views.DeleteCoffee.as_view(), name='delete_coffee'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
